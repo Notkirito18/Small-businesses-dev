6ddb88770e72b620d6c3033712cec81dba7fc505
Notkirito18 <djallel123451@gmail.com>
2025-06-04 22:34:43 +0200

    tabs change

--- js/tabs.js
+++ js/tabs.js
@@ -1,5 +1,5 @@
 //  "#27debf"
-//  "#333A73"
+//  "#F3C623"
 //  "#C499F3"
 //  "#FF004D"
 
@@ -26,113 +26,77 @@ function tabClicked(tab) {
 
   contentSelected.classList.add("show");
 }
-function addGradient(element) {
-  element.style.color = "#ffffff";
-  switch (element.id) {
-    case "travel":
-      element.style.background = "#216136";
-      break;
-    case "sports":
-      element.style.background = "#181d42";
-      break;
-    case "education":
-      element.style.background = "#66438b";
-      break;
-    case "events":
-      element.style.background = "#7a1131";
-      break;
-  }
-}
-function removeGradient(element) {
-  if (element.classList.length > 1) {
-    switch (element.id) {
-      case "travel":
-        element.style.background = "#27debf";
-        break;
-      case "sports":
-        element.style.background = "#333A73";
-        break;
-      case "education":
-        element.style.background = "#C499F3";
-        break;
-      case "events":
-        element.style.background = "#FF004D";
-        break;
-    }
-  } else {
-    element.style.background = "#032144";
-  }
-}
-document.getElementById("travel").addEventListener("click", () => {
-  travelClick();
+// function addGradient(element) {
+//   element.style.color = "#ffffff";
+//   switch (element.id) {
+//     case "first-project-btn":
+//       element.style.background = "#216136";
+//       break;
+//     case "second-project-btn":
+//       element.style.background = "#181d42";
+//       break;
+//     case "third-project-btn":
+//       element.style.background = "#66438b";
+//       break;
+//     case "fourth-project-btn":
+//       element.style.background = "#7a1131";
+//       break;
+//   }
+// }
+// function removeGradient(element) {
+//   if (element.classList.length > 1) {
+//     switch (element.id) {
+//       case "first-project-btn":
+//         element.style.background = "#27debf";
+//         break;
+//       case "second-project-btn":
+//         element.style.background = "#F3C623";
+//         break;
+//       case "third-project-btn":
+//         element.style.background = "#C499F3";
+//         break;
+//       case "fourth-project-btn":
+//         element.style.background = "#FF004D";
+//         break;
+//     }
+//   } else {
+//     element.style.background = "#032144";
+//   }
+// }
+document.getElementById("first-project-btn").addEventListener("click", () => {
+  firstProjectBtnClick();
 });
-document.getElementById("travel").addEventListener("touchstart", () => {
-  travelClick();
-});
+document
+  .getElementById("first-project-btn")
+  .addEventListener("touchstart", () => {
+    firstProjectBtnClick();
+  });
 
-travelClick = () => {
-  document.getElementById("travel").style.background = "#27debf";
-  document.getElementById("sports").style.background = "#032144";
-  document.getElementById("education").style.background = "#032144";
-  document.getElementById("events").style.background = "#032144";
+firstProjectBtnClick = () => {
+  document.getElementById("first-project-btn").style.background = "#27debf";
+  document.getElementById("second-project-btn").style.background = "#032144";
+  document.getElementById("third-project-btn").style.background = "#032144";
+  document.getElementById("fourth-project-btn").style.background = "#032144";
 };
 
-document.getElementById("sports").addEventListener("click", () => {
-  document.getElementById("sports").style.background = "#333A73";
-  document.getElementById("travel").style.background = "#032144";
-  document.getElementById("education").style.background = "#032144";
-  document.getElementById("events").style.background = "#032144";
+document.getElementById("second-project-btn").addEventListener("click", () => {
+  document.getElementById("second-project-btn").style.background = "#F3C623";
+  document.getElementById("first-project-btn").style.background = "#032144";
+  document.getElementById("third-project-btn").style.background = "#032144";
+  document.getElementById("fourth-project-btn").style.background = "#032144";
 });
-document.getElementById("education").addEventListener("click", () => {
-  document.getElementById("education").style.background = "#C499F3";
-  document.getElementById("sports").style.background = "#032144";
-  document.getElementById("travel").style.background = "#032144";
-  document.getElementById("events").style.background = "#032144";
+document.getElementById("third-project-btn").addEventListener("click", () => {
+  document.getElementById("third-project-btn").style.background = "#C499F3";
+  document.getElementById("second-project-btn").style.background = "#032144";
+  document.getElementById("first-project-btn").style.background = "#032144";
+  document.getElementById("fourth-project-btn").style.background = "#032144";
 });
-document.getElementById("events").addEventListener("click", () => {
-  document.getElementById("events").style.background = "#FF004D";
-  document.getElementById("sports").style.background = "#032144";
-  document.getElementById("education").style.background = "#032144";
-  document.getElementById("travel").style.background = "#032144";
+document.getElementById("fourth-project-btn").addEventListener("click", () => {
+  document.getElementById("fourth-project-btn").style.background = "#FF004D";
+  document.getElementById("second-project-btn").style.background = "#032144";
+  document.getElementById("third-project-btn").style.background = "#032144";
+  document.getElementById("first-project-btn").style.background = "#032144";
 });
-
-// //* Services nav dropdown menue
-// document
-//   .getElementById("services-dropdown-title")
-//   .addEventListener("click", () => {
-//     if (window.screen.width > 768) {
-//       if (
-//         document.getElementById("services-dropdown").style.display == "none"
-//       ) {
-//         document.getElementById("services-dropdown").style.display = "flex";
-//       } else {
-//         document.getElementById("services-dropdown").style.display = "none";
-//       }
-//     } else {
-//       if (document.getElementById("sm-dropdown").style.display == "none") {
-//         document.getElementById("sm-dropdown").style.display = "block";
-//       } else {
-//         document.getElementById("sm-dropdown").style.display = "none";
-//       }
-//     }
-//   });
-// document.addEventListener("click", function (event) {
-//   if (
-//     !document.getElementById("services-dropdown").contains(event.target) &&
-//     !document
-//       .getElementById("services-dropdown-title")
-//       .contains(event.target) &&
-//     document.getElementById("services-dropdown").style.display != "none"
-//   ) {
-//     document.getElementById("services-dropdown").style.display = "none";
-//   }
-//   if (
-//     !document.getElementById("sm-dropdown").contains(event.target) &&
-//     !document
-//       .getElementById("services-dropdown-title")
-//       .contains(event.target) &&
-//     document.getElementById("sm-dropdown").style.display != "none"
-//   ) {
-//     document.getElementById("sm-dropdown").style.display = "none";
-//   }
-// });
+$(document).ready(function () {
+  firstProjectBtnClick();
+});
